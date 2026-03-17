f5443c3322c74e27759ec889b79d3ae9dbb6882d
aittrial <israhorse@gmail.com>
2026-03-17 13:27:54 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Float, Date
+from database import Base
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float, nullable=False)
+    description = Column(String, nullable=False)
+    date = Column(Date, nullable=False)
