493bca3662c270c670636972fa86efc7cd63ff98
aittrial <israhorse@gmail.com>
2026-03-18 16:23:38 +0200

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from database import Base
 
+
 class Expense(Base):
     __tablename__ = "expenses"
     id = Column(Integer, primary_key=True, index=True)
